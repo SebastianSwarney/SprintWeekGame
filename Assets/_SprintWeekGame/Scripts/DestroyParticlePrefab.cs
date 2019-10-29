d8c1d785998871928f879d3215a2e16453299f27
Amelia-McLeavey <57147615+Amelia-McLeavey@users.noreply.github.com>
2019-10-29 15:31:06 -0400

    Add wall particle system

---
+++ Assets/_SprintWeekGame/Scripts/DestroyParticlePrefab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticlePrefab : MonoBehaviour
+{
+    ParticleSystem partSyst;
+
+    private void Start()
+    {
+        partSyst = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (partSyst)
+        {
+            if(!partSyst.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
