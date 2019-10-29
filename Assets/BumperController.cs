543f93ef13df9eb38c93170184e91c5cf461c361
DrewGodden <35704776+DrewGodden@users.noreply.github.com>
2019-10-29 13:40:58 -0400

    Added new test scene

---
+++ Assets/BumperController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperController : MonoBehaviour
+{
+    public LayerMask m_mask;
+
+    public float force;
+    public float forceRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void PushFromCenter()
+    {
+        Collider2D[] overlappingColliders = Physics2D.OverlapCircleAll(transform.position, forceRange, m_mask);
+
+        foreach (Collider2D collider in overlappingColliders)
+        {
+            collider.GetComponent<Rigidbody2D>().AddForce(collider.transform.position - transform.position * force, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PushFromCenter();
+    }
+}
