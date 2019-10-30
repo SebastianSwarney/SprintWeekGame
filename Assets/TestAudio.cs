8b11f4710044ef7ebed1778d69c300c718fc3e02
DrewGodden <35704776+DrewGodden@users.noreply.github.com>
2019-10-30 12:09:47 -0400

    Added test audio

---
+++ Assets/TestAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAudio : MonoBehaviour
+{
+    AudioSource audioSourceComponent;
+    public AudioClip backgroundMusic;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSourceComponent = gameObject.GetComponent<AudioSource>();
+        audioSourceComponent.PlayOneShot(backgroundMusic);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
